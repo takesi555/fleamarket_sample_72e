03f5f10ecd5c259d4c0a54cdef5fbe8e6d4a7d39
NaokiSakaiRose <naoki1777sakaibara@gmail.com>
2020-06-21 13:52:16 +0900

    ReviseDB_ReviseGemfile

--- db/migrate/20200620193551_create_categories.rb
+++ db/migrate/20200620193551_create_categories.rb
@@ -1,8 +1,8 @@
 class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
-      t.string :name,    null: false
-      t.string :path
+      t.string  :name,     null: false
+      t.integer :ancestry, index: true
     end
   end
 end
