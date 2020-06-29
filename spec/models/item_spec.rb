8e15422481adf07fc92095631ba3f5bd563a3070
YoshidaNorifum1 <flavor.of.coffee.and.cigar@gmail.com>
2020-06-29 13:42:10 +0900

    delete coffees and add tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,7 +3,18 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
   describe '#confirm' do
-    it "" do
+    it "can register a card" do
+    end
+    it "can delete a card" do
+    end
+    it "can delete a card if the owner deleted" do
+    end
+    it "can redirect to toppage if it's purchase succsess" do
+    end
+
+    it "can't register if the card is invalid" do
+    end
+    it "can't purchase if no cards registerd" do
     end
   end
 end
