77f646e13dd626c6688a9548687f86343cc5b01e
YoshidaNorifum1 <flavor.of.coffee.and.cigar@gmail.com>
2020-06-29 13:57:02 +0900

    Create creditcard controller and make a route

---
+++ app/controllers/creditcards_controller.rb
@@ -0,0 +1,8 @@
+class CreditcardsController < ApplicationController
+  def new
+  end
+
+  def create
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index'
+
+  resources :creditcards ,only: [:new,:create]
   resources :items do
     member do
       get 'confirm'
