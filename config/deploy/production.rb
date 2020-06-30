6ecd500d1e0368db86fc6c9fbe0beff6dc7ef47c
suzuki19921215 <suzuki19921215@gmail.com>
2020-06-30 23:48:59 +0900

    BASIC_authentication

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,8 @@
 server '13.113.127.100', user: 'ec2-user', roles: %w{app db web}
 # 自動デプロイに際して必要な動作
+
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
+
+# role-based syntax
+# ==================
