1449c2628f7f91f53f6c3514ae6dcf4ad8fb3617
ak-2015 <akiko_torii@hotmail.com>
2020-07-05 16:05:09 +0900

    edit model - Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+  belongs_to :brand
+  belongs_to :prefecture
+  has_many :itemimages, dependent: :destroy
+
+  with_options presence: true do
+    validates :itemimages, presence: true, length: { in: 1..10 }
+    validates :name
+    validates :description
+    validates :category
+    validates :condition
+    validates :postage
+    validates :prefecture
+    validates :preparation
+    validates :price
+    validates :user
+    validates :status
+  end
 end
