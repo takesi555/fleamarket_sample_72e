5c68070c56a9f2b65a092d38fd9487778dacefd0
ak-2015 <akiko_torii@hotmail.com>
2020-07-11 16:16:52 +0900

    added association between Item and Condition, Postage, Preparation

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,12 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   has_many :itemimages, dependent: :destroy
   accepts_nested_attributes_for :itemimages, allow_destroy: true
-
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :preparation
+  
   with_options presence: true do
     validates :itemimages, presence: true, length: { in: 1..10 }
     validates :name
