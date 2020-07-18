01e9a55de41765cfbf6d6f5fb9df341d99ed3f2e
ak-2015 <akiko_torii@hotmail.com>
2020-07-18 14:43:28 +0900

    created an action - #items/create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,15 @@ class ItemsController < ApplicationController
   end
 
   def create
+    item = Item.new(item_params)
+    if item.save
+      redirect_to root_path
+    else
+      redirect_to new_item_path
+    end
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :size, :brand_id, :condition_id, :postage_id, :prefecture_id, :preparation_id, :price, itemimages_attributes: [:image]).merge(user_id: 1, status: 1)
+  end
   end
 end
