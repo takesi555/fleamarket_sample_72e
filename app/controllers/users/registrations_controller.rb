2636e0bcdeb511679579471969132dd0870d9625
YoshidaNorifum1 <62142890+YoshidaNorifum1@users.noreply.github.com>
2020-07-20 22:27:38 +0900

    Revert "Revert "マイページ""

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,43 +5,66 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  def new
-    super
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource
-  def create
-    params[:user][:birthday] = params[:birthday]
-    @user = User.new(sign_up_params)
-      unless @user.valid?
-      flash.now[:alert] = @user.errors.full_messages
-      render :new and return
-    end
-    session["devise.regist_data"] = {user: @user.attributes}
-    session["devise.regist_data"][:user]["password"] = params[:user][:password]
-    @destination = @user.build_destination
-    render :new_destination
+
+  def new
+    @user = User.new
   end
 
-  def create_destination
-    @user = User.new(session["devise.regist_data"]["user"])
-    @destination = Destination.new(destination_params)
-    unless @destination.valid?
-      flash.now[:alert] = @destination.errors.full_messages
-      render :new_destination and return
+  def create
+    @user = User.new(sign_up_params) #登録1ページ目から送られてきたパラメータを@userに代入
+    unless @user.valid? #validメソッドを使ってバリデーションチェック
+      flash.now[:alert] = @user.errors.full_messages
+      render :new and return #and returnを使って条件分岐を明示的に終了させている。
     end
-    @user.build_destination(@destination.attributes)
+   
+    session["devise.regist_data"] = {user: @user.attributes} 
+    #sessionにハッシュオブジェクトで情報保持させるため、attributesメソッドでデータ整形。
+
+    session["devise.regist_data"][:user][:password] = params[:user][:password] 
+    #前述で含まれなかった情報を代入。
+    #[:user]["password"]となっていたのを[:user][:password]に書き換えました。
+    #ネットでは、sessionに関して、ネスト構造になっている場合は、2階層目以降を文字列表記にする必要があるとの噂がありましたが、両方ともシンボル型でOKとのこと。
+    #要は、前述のコードがシンボル型ならこの箇所もシンボル型に、文字列ならこの箇所も文字列に、合わせる必要がある。
+
+    #params[:user][:password]において、paramsの中身（userモデルのpasswordカラム）から値を取り出している。ネスト構造な書き方。
+    # （params[:password]ではなく、params[:user][:password]という書き方で、モデル・カラムという順番で指定している）
+    #その値をsession["devise.regist_data”]の中の[:user]というハッシュの[“password”]というキーのバリューに代入している。
+
+    @address = @user.build_ship_address 
+    #build_ship_addressメソッドはhas_one :ship_addressのアソシエーションを設定すると使用可。関連づけのあるnewメソッドのようなもの。
+    render :new_address #登録2ページ目に遷移
+  end
+
+  def create_address
+    @user = User.new(session["devise.regist_data"]["user"])
+    #session["devise.regist_data”]の中の["user”]というハッシュの情報を@userに代入している。
+
+    #user、という同じハッシュがアクションをまたぐ（create~create_address）と、create_addressアクションにおいては自動的に文字列になってしまう。
+    #binding.pryで検証すると確認できるとのこと。
+    #そのため文字列表記で指定しないといけない。
+
+    @address = ShipAddress.new(address_params)
+    unless @address.valid?
+      flash.now[:alert] = @address.errors.full_messages
+      render :new_address and return
+    end
+    @user.build_ship_address(@address.attributes) #送られてきたparamsをbuild_ship_addressを用いて@userに代入
     @user.save
-    session["devise.regist_data"]["user"].clear
-    sign_in(:user, @user)
+    session["devise.regist_data"]["user"].clear #sessionを明示的に削除 セキュリティ上必要
+    sign_in(:user, @user) #登録後ログイン状態になるようにしている
+    redirect_to complete_users_path
   end
 
   protected
-
-  def destination_params
-    params.require(:destination).permit(:post_code, :city, :block_num, :bill_name, :prefecture_id, :first_name, :last_name, :first_name_kana, :last_name_kana, :tel)
+   
+  def address_params
+    params.require(:ship_address).permit(:last_name, :first_name, :ruby_last_name, :ruby_first_name, :postal_code, :prefectures, :city, :address_detail, :apartment_name, :room_number, :phone_number)
   end
-
 
   # GET /resource/edit
   # def edit
@@ -67,7 +90,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-
+  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-  }
-  devise_scope :user do
-    get 'destinations', to: 'users/registrations#new_destination'
-    post 'destinations', to: 'users/registrations#create_destination'
-  end
+  devise_for :users
   root to: 'products#index'
+  resources :items, only: [:new, :create]
   resources :users, only: [:show]
-  resources :registration, only: [:index]
-  resources :creditcards, only: [:new, :create,:destroy]
-  resources :items ,only: [:new, :create] do
-    member do
-      get 'confirm'
-      post 'purchase'
-    end
-  end
 end
+
