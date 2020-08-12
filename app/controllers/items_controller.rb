b75a30debfadb317aabb5192a3bf70ef642d0f9f
suzuki19921215 <suzuki19921215@gmail.com>
2020-08-12 21:31:36 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,6 @@ class ItemsController < ApplicationController
   before_action :set_show, only: [:edit, :update, :show, :destroy]
 
   def index
-    #@items = Post.where.(category_id: 1..10).order('created_at DESC').limit(10).where.not(condition: 1).where(condition: 0)
     @items = Item.where.not(status: '2')
     @parents = Category.where(ancestry: nil)
   end
